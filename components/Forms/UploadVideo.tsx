e96091ef0397686e78c29818d3346f649ee2d79d
Nachobr <55702500+Nachobr@users.noreply.github.com>
2021-12-06 16:52:07 -0300

    feat: style upload video comp

--- components/Forms/UploadVideo.tsx
+++ components/Forms/UploadVideo.tsx
@@ -55,28 +55,30 @@ export default function UploadVideo({ formStep, nextFormStep }) {
                         direction="column"
                         justifyContent="center"
                         alignItems="center" 
-                        style={{display: "flex", width: "846px", position: "relative", left: "470px", alignItems: "flex-start"}}>
-                          <ListItemText disableTypography style={{display: "flex", justifyContent: "center", fontWeight:"600", fontFamily: "Open Sans, sans-serif", marginTop: "15px"}}> Instructions to record the video: </ListItemText>
+                        style={{display: "flex", width: "846px", position: "relative", left: "470px", top: "-40px", alignItems: "flex-start"}}>
+                          <ListItemText disableTypography style={{display: "flex", justifyContent: "center", fontWeight:"600", fontFamily: "Open Sans, sans-serif", marginBottom: "15px"}}> Instructions to record the video: </ListItemText>
                           <ul>
-                            <li>The sign should display in a readble manner the full Ethereum address of the submitted (No ENS; no ellipsis)</li>
-                            <li>The sign can be a screen. The submitted must show the sign in the right orientation to be read on the video</li>
-                            <li>The submitter must say (in English), "I certify that I am a real human and that I am not already registered in this registry".</li>
-                            <li>Speak in your normal voice</li>
-                            <li>Make sure you're facing the camera</li>
-                            <li>Make sure you're facial features are visible</li>
+                            <li>• The sign should display in a readble manner the full Ethereum address of the submitted (No ENS; no ellipsis)</li>
+                            <li>• The sign can be a screen. The submitted must show the sign in the right orientation to be read on the video</li>
+                            <li>• The submitter must say (in English), "I certify that I am a real human and that I am not already registered in this registry".</li>
+                            <li>• Speak in your normal voice</li>
+                            <li>• Make sure you're facing the camera</li>
+                            <li>• Make sure you're facial features are visible</li>
                           </ul>
+                          <ListItemText  style={{color: "#FF9900", marginBottom: "20px", marginTop: "-5px"}}> Check example 1, and exmple 2 </ListItemText> 
+
 
                           <h3>Video Requirements</h3>
-                          <ul>
-                            <li>Less than 2 minutes long</li>
-                            <li>video/webm, video/mp4, video/avi or video/mov format</li>
-                            <li>Vertical (portrait), horizontal (landscape), or square</li>
+                          <ul style={{color: "#979797"}}>
+                            <li>• Less than 2 minutes long</li>
+                            <li>• Video/webm, video/mp4, video/avi or video/mov format</li>
+                            <li>• Vertical (portrait), horizontal (landscape), or square</li>
                           </ul>
 
                           <h3>Minimum size Requirements</h3>
-                          <ul>
-                            <li>Minimum height: equal to or higher than 352 pixels</li>
-                            <li>Minimum width: equal to or higher than 352 pixels</li>
+                          <ul style={{color: "#979797"}}>
+                            <li>• Minimum height: equal to or higher than 352 pixels</li>
+                            <li>• Minimum width: equal to or higher than 352 pixels</li>
                           </ul>
               </Grid>
                 <form style={{position: "relative", display: "flex", top: "13px", left: "70px"}} onSubmit={handleSubmit(onSubmit)}>
